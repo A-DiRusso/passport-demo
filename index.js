bb7760bb837cbbb3bc1efd10b7ea6a1ddaa3d138
Anthony DiRusso <ossurid@gmail.com>
2019-04-09 10:55:35 -0400

    completes set up of Oauth

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 //First bring in dotenv
 require('dotenv').config();
 const PORT = process.env.PORT;
+const session = require('express-session');
 const express = require('express');
 const app = express();
+const FileStore = require('session-file-store')(session);
+const setupAuth = require('./auth');
+// const S3KRET = require('./config');
+
+// tell express to use the session modules
+app.use(session({
+    store: new FileStore(),  // no options for now
+    secret: process.env.SESSION_SECRET
+}));
+
+//only after we have set up sessions with express 
+//is it okay to attach passport
+//authentication
+setupAuth(app);
+
+
+
 app.get('/', (req, res) => {
     res.send(`
     <h1>Hello There</h1>
